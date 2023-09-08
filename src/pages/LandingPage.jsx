19a2230ace0519e56e86bf33a0dd22de0aa7d2b9
Jeimyre Gale Perez <jeimyregaleperez@gmail.com>
2023-09-08 15:21:07 +0800

    initial commit

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,49 @@
+import '../style/LandingPage.css';
+import photo from '../../public/Gale.png'
+
+function LandingPage() {
+    return (
+        <>
+            <div className='front'>
+                <div className='photo'>
+                    <img src={photo} />
+                </div>
+                <div>
+                    <h1>Gale's PPortfolio</h1>
+                </div>
+            </div>
+            <div>
+
+            </div>
+            <div class="background">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </>
+    )
+}
+
+export default LandingPage
